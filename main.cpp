a5084fefec7ce0fcabb991d5528df8485aa27811
Vlad Guryev <teylor-world@yandex.ru>
2019-03-12 15:31:40 +0300

    Implementation of ring buffer with std::deque, also bug with size() count fixed the name and email changed

--- main.cpp
+++ main.cpp
@@ -6,29 +6,14 @@ using namespace std;
 int main()
 {
     const int N = 5;
-    RingBuffer<int, N> buffer;
+    RingBuffer<int> buffer(N);
     for (int i = 0; i < 20 * N; i++) {
         buffer.push_back(i);
     }
-    ASSERT_EQUAL(buffer[0], 95);
-    ASSERT_EQUAL(buffer[1], 96);
-    ASSERT_EQUAL(buffer[2], 97);
-    ASSERT_EQUAL(buffer[3], 98);
-    ASSERT_EQUAL(buffer[4], 99);
 
-    RingBuffer<int, 4> buffer2;
-    buffer2.push_back(1);
-    buffer2.push_back(1);
-    ASSERT_EQUAL(buffer2.size(), 2);
-    ASSERT_EQUAL(buffer2.capacity(), 4);
-    buffer2.push_back(1);
-    ASSERT_EQUAL(buffer2.size(), 3);
-    buffer2.push_back(1);
-    ASSERT_EQUAL(buffer2.size(), 4);
-    buffer2.push_back(1);
-    ASSERT_EQUAL(buffer2.size(), 4);
-    ASSERT_EQUAL(buffer2.capacity(), 4);
-    cout << "tests passed" << endl;
+    for(const auto& item : buffer)
+        cout << item << " ";
+    cout << endl;
 
     return 0;
 }
